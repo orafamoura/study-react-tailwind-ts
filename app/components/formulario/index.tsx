03083aad083edb77b354a8ac789f40f41ffc0ac6
orafamoura <raphaelmvb2@gmail.com>
2025-06-13 15:18:03 -0300

    só layout

--- app/components/formulario/index.tsx
+++ app/components/formulario/index.tsx
@@ -6,24 +6,24 @@ import ButtonNormal from '../buttons';
 const Formulario = () => {
   return (
     <div className='w-full max-w-xs'>
-        <form className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'> 
+        <form className='bg-white inset-shadow-md rounded px-8 pt-6 pb-8 mb-4'> 
             <div className='mb-4'>
                 <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='tarefa'>
                  Adicione alguma coisa nova
                 </label>
-                    <input className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                <input className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
                     type='text'
                     name='tarefa'
                     id='tarefa'
                     placeholder='O que você quer estudar?'
                     required
-                    />
-
-            <div className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
-                <label>
-                Tempo
+                />
+            <div>
+            <div className='mb-6'>
+                <label className='block text-gray-700 text-sm font-bold mb-2'>
+                    Tempo
                 </label>
-                    <input
+                <input
                     className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                     type='time'
                     step='1'
@@ -32,7 +32,8 @@ const Formulario = () => {
                     min='00:00:00'
                     max='01:30:00'
                     required
-                    />
+                />
+            </div>
             </div>
             <ButtonNormal/>
             </div>
