3f0391ba095359661e252d555cab950fcfc40baa
orafamoura <raphaelmvb2@gmail.com>
2025-06-23 17:17:25 -0300

    montando o botão

--- app/components/formulario/index.tsx
+++ app/components/formulario/index.tsx
@@ -1,46 +1,54 @@
 'use client';
 
-import React from 'react';
+import React, { useRef } from 'react';
 import Button from '../buttons';
 
 const Formulario = () => {
-  return (
-    <div className='w-full'>   
-        <form className='flex flex-wrap bg-gray-300 inset-shadow-md rounded px-8 pt-6 pb-8 mb-4'> 
-            <div className='m-4'>
-                <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='tarefa'>
-                 Adicione alguma coisa nova
-                </label>
-                <input className='shadow-lg appearance-none border rounded w-full mb-3 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-                    type='text'
-                    name='tarefa'
-                    id='tarefa'
-                    placeholder='O que você quer estudar?'
-                    required
-                />
-            </div>
-            <div className='m-4'>
-                <label className='block text-gray-700 text-sm font-bold mb-2'>
-                    Tempo
-                </label>
-                <input
-                    className="shadow-lg appearance-none border rounded w-full mb-3 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    type='time'
-                    step='1'
-                    name='tempo'
-                    id='tempo'
-                    min='00:00:00'
-                    max='01:30:00'
-                    required
-                />
-            </div>
-            <div className='flex justify-center'>
-            <Button>adicionar</Button>
-            </div>
-        </form> 
-    </div>
 
-  );
+    const inputRef = useRef(null)
+
+    function criarCardLista(){  
+        console.log(inputRef)
+    }
+
+    return (
+        <div className='w-full'>   
+            <form className='flex flex-wrap bg-gray-300 inset-shadow-md rounded px-8 pt-6 pb-8 mb-4'> 
+                <div className='m-4'>
+                    <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='tarefa'>
+                    Adicione alguma coisa nova
+                    </label>
+                    <input className='shadow-lg appearance-none border rounded w-full mb-3 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                        type='text'
+                        name='tarefa'
+                        id='tarefa'
+                        placeholder='O que você quer estudar?'
+                        ref={inputRef}
+                        required
+                    />
+                </div>
+                <div className='m-4'>   
+                    <label className='block text-gray-700 text-sm font-bold mb-2'>
+                        Tempo
+                    </label>
+                    <input
+                        className="shadow-lg appearance-none border rounded w-full mb-3 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        type='time'
+                        step='1'
+                        name='tempo'
+                        id='tempo'
+                        min='00:00:00'
+                        max='01:30:00'
+                        required
+                    />
+                </div>
+                <div className='flex justify-center'>
+                <Button onClick={criarCardLista}>adiciona</Button>
+                </div>
+            </form> 
+        </div>
+
+    );
 };
 
 export default Formulario;
--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,15 @@ import Cronometro from './components/Cronometro';
 function App() {
   return (
     <div className="grid grid-rows-(--min-content min-content auto) bg-gray-500 justify-items-center">
-        <div className='bg-current p-4'>
-            <Formulario />
-            <Cronometro />    
-            <Lista />
+        <div className='flex flex-wrap p-4'>
+            <div className='w-full lg:w-1/2 p-2'>
+                <Formulario />
+                <Cronometro /> 
+            </div>
+            <div className='w-full lg:w-1/2 p-2'>
+                <Lista />
+            </div>
+   
         </div>
     </div>
   );
